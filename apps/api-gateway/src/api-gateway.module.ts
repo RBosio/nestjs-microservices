57a816ff40847d149d6fdaec297c1e58b1d0b781
Fiido <roccobosio@gmail.com>
2024-03-15 16:21:54 -0300

    nats api-gateway connection

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ApiGatewayController } from './api-gateway.controller';
 import { ApiGatewayService } from './api-gateway.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'NATS_SERVICE',
+        transport: Transport.NATS,
+        options: {
+          servers: ['nats://nats'],
+        },
+      },
+    ]),
+  ],
   controllers: [ApiGatewayController],
   providers: [ApiGatewayService],
 })
