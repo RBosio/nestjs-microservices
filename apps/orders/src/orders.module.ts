87c4d3367137071d22f83d8e7722d978e927b5c9
Fiido <roccobosio@gmail.com>
2024-03-15 17:14:22 -0300

    orders microservice connected to database

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { DatabaseModule } from '@app/common';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MYSQL_URI: Joi.string().required(),
+        MYSQL_SYNCHRONIZE: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
