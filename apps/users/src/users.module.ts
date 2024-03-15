0eabe59ed8c0f1b369334e850a90b41f39793a31
Fiido <roccobosio@gmail.com>
2024-03-15 17:14:11 -0300

    users microservice connected to database

--- apps/users/src/users.module.ts
+++ apps/users/src/users.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { DatabaseModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MYSQL_URI: Joi.string().required(),
+        MYSQL_SYNCHRONIZE: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
