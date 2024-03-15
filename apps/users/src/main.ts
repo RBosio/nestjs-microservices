d9a44005bd1446d794ffe3fbb64a69a73c8879d2
Fiido <roccobosio@gmail.com>
2024-03-15 16:22:25 -0300

    nats users connection

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { UsersModule } from './users.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(UsersModule);
-  await app.listen(3000);
+
+  app.connectMicroservice({
+    transport: Transport.NATS,
+    options: {
+      servers: ['nats://nats'],
+    },
+  });
+
+  app.startAllMicroservices();
 }
 bootstrap();
